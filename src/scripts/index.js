8fd927f8fc70cad815e23a5507d33938a2c13c7d
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-02 15:11:51 -0300

    add function randomColor and repositorys, refactoring style css

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,15 @@
 document.getElementById("btn-search").addEventListener("click", () => {
   const userName = document.getElementById("input-search").value;
-  getUserProfile("EduardoMG12");
+  getUserProfile(userName);
+  getRepositories(userName);
+});
+
+document.getElementById("input-search").addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    const userName = document.getElementById("input-search").value;
+    getUserProfile(userName);
+    getRepositories(userName);
+  }
 });
 
 const user = async (userName) => {
@@ -8,14 +17,46 @@ const user = async (userName) => {
   return await response.json();
 };
 
+const repositorys = async (userName) => {
+  const response = await fetch(`https://api.github.com/users/${userName}/repos`);
+  return await response.json();
+};
+
 const getUserProfile = (userName) => {
   user(userName).then((userData) => {
-    let userInfo = `<img src="${userData.avatar_url}" alt="foto de perfil do usuário" />
-                    <div class="data">
+    let userInfo = `<div class="info">
+                      <img src="${userData.avatar_url}" alt="foto de perfil do usuário" />
+                      <div class="data">
                         <h1>${userData.name ?? "Não possui nome cadastrada 😢"}</h1>
                         <p>${userData.bio ?? "Não possui bio cadastrada 😢"}</p>
+                      </div>
+                      </div>
 
 `;
     document.querySelector(".profile-data").innerHTML = userInfo;
   });
+};
+const getRepositories = (userName) => {
+  repositorys(userName).then((repositoysData) => {
+    let repositorysItens = "";
+    repositoysData.forEach((repo) => {
+      repositorysItens += `<li><a href="${repo.html_url}" target="_blank" style="background-color:${randomColor()}">${
+        repo.name
+      }</a> </li>`;
+    });
+    document.querySelector(".profile-data").innerHTML += `
+                                                          <div class="repositories section">
+                                                            <h2>Repositórios</h2>
+                                                            <ul>${repositorysItens}<ul>
+                                                          </div>
+                                                          `;
+  });
+};
+const randomColor = () => {
+  let characteres = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += characteres[Math.floor(Math.random() * 16)];
+  }
+  return color;
 };
